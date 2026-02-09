f12c1e7fae9722b7a0f404436e777a6cce4619be
rodolphe2006 <rodolphebahati@gmail.com>
2026-02-09 23:18:21 +0100

    tryna fixing the problem with port

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 import express from "express";
-import allGenres from "./routes/allGenres.js";
+import dotenv from "dotenv";
+import allGenres from "./routes/allgGenres.js";
 import createGenre from "./routes/createGenre.js";
 import genreByid from "./routes/genreById.js";
 import editGenre from "./routes/editGenre.js";
 import deleteGenre from "./routes/deleteGenre.js";
+
+dotenv.config();
+
 const app = express();
 const preFix = "/Vidvaldi";
+
+const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(preFix, allGenres);
 app.use(preFix, createGenre);
 app.use(preFix, genreByid);
 app.use(preFix, editGenre);
 app.use(preFix, deleteGenre);
-app.listen(5000);
+app.listen(port);
